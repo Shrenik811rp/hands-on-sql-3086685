37f517b5eb21689bbf583c727585e2cdd5125e3e
Shrenik <61610570+Shrenik811rp@users.noreply.github.com>
2023-10-26 09:48:37 +0000

    select statement

--- queries/01_01/SELECTStatement.sql
+++ queries/01_01/SELECTStatement.sql
@@ -1,5 +1,9 @@
 SELECT * FROM emp_tab;
 
+-- command to list column names
+PRAGMA table_info(emp_tab);
+
 SELECT empno,name FROM emp_tab;
 
 SELECT empno,name,salary*12 'Annual Salary' FROM emp_tab;
+
